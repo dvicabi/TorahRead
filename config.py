78433c80293bea499a75c1740b309ef544c485ca
Dvir Cabessa <dvicabi@gmail.com>
2025-06-06 01:31:19 +0300

    organize config folder and update .gitignore to exclude sensitive files

--- config.py
+++
@@ -1,8 +0,0 @@
-SCOPES = [
-    "https://www.googleapis.com/auth/youtube.upload",
-    "https://www.googleapis.com/auth/youtube.force-ssl"
-]
-
-VIDEO_DIR = "פרשות השבוע"
-THUMBNAIL_EXT = ".png"
-VIDEO_EXT = ".mov"
